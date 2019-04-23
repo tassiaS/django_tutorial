8e57d4b89f663ffe870435d30faf1a4e28c50f73
Tassia Castro <tassiaserrao@gmail.com>
2019-04-23 17:50:06 +0200

    Removed lost password html and some comments

--- polls/urls.py
+++ polls/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('<int:question_id>', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('<int:question_id>/message/', views.message, name='message'),
     path('new_question/', views.new_question, name='new_question'),
     path('save_question/', views.save_question, name='save_question'),
     path('new_user/', views.new_user, name='new_user'),
--- polls/views.py
+++ polls/views.py
@@ -16,33 +16,22 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    # context = {
-    #     'latest_question_list': latest_question_list
-    # }
-    return HttpResponse(template.render(context, request))
-
 @login_required
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     current_user = request.user
-    print(current_user.id)
+    
     # If the user logged in created the question, dont allow he/she to answer it
     if current_user.id == question.user_id:
         return HttpResponse("You can't answer your own question =)")
     else:
         return render(request, 'polls/detail.html', {'question': question})
         
-    # try:
-    #     question = Question.objects.get(pk= question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
 @login_required
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
 @login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -56,19 +45,8 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+        
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-@login_required
-def message(request, question_id):
-    candidate_message = request.POST["body_of_message"]
-    question = get_object_or_404(Question, pk=question_id)
-    question.message_set.create(candidate_message=candidate_message)
-    # question.save()
-    # return HttpResponse(candidate_message)
-
-    return HttpResponseRedirect(reverse("polls:detail", args=(question.id,)))
 
 @login_required
 def new_question(request):
@@ -102,9 +80,9 @@ def save_user(request):
 
     current_user = User.objects.create_user(username=request.POST['username'],
                                             password=request.POST['psw'])
-    user.save()
+    current_user.save()
     
-    return render(request, 'polls/index.html')
+    return HttpResponseRedirect(reverse("polls:index"))
 
 def this_is_json(request):
     return JsonResponse({"message":"Hello class", 
